f01b7fb59cac58509057811939428a430ceec6ba
Bianca Zanol <biancabzoliveira@gmail.com>
2021-12-08 11:11:53 -0300

    atualizaçoes 08.01

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 
-app.get("/", (request,response)=>{
-    return response.json({message:"nova mensagem"})
+app.get('/', (request, response) => {
+  return response.json({ message: 'nova mensagem' });
 });
 
-app.listen(8000);
+app.listen(8000, () => {
+  console.log('Servidor em funcionamento!');
+});
+
+module.exports = app;
